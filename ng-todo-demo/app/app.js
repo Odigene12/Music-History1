5d2efc0c6503a4fb87bdc9d2b2cbdd05c202b176
Odie Joseph <Odie Joseph>
2016-05-21 10:14:27 -0500

    added functions

--- ng-todo-demo/app/app.js
+++ ng-todo-demo/app/app.js
@@ -7,4 +7,24 @@ app.controller("NavCtrl", function($scope){
 
 app.controller("TodoCtrl", function($scope){
 	$scope.welcome = "hello";
+	$scope.showListView = true;	
+
+	$scope.newItem = function() {
+		console.log("you clicked newItem");
+		$scope.showListView = true;
+	};
+	$scope.allItem = function(){
+		console.log("you clicked allItem");
+		$scope.showListView = false; 
+	};
+
+
+
+
+
+
+
+
+
+
 });
